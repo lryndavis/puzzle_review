5c50e160789721ab78fe8c3aca4e49c4414438c4
Lauryn Davis <lauryndavis@tests-MacBook-Pro.local>
2016-01-08 09:03:07 -0800

    creates and passes specs to handle other vowels and capitalized characters

--- lib/puzzle.rb
+++ lib/puzzle.rb
@@ -4,8 +4,17 @@ class String
     new_word = self.split("")
     for character in 0..new_word.length() -1
       letter = new_word[character]
-      if letter == ("a")
+      # letter.downcase!()
+      if letter == ("a") || letter == ("A")
         new_phrase.push("-")
+      elsif letter == ("e") || letter == ("E")
+        new_phrase.push("-")
+      elsif letter == ("i") || letter == ("I")
+      new_phrase.push("-")
+      elsif letter == ("o") || letter == ("O")
+      new_phrase.push("-")
+      elsif letter == ("u") || letter == ("U")
+      new_phrase.push("-")
       else
         new_phrase.push(letter)
       end
--- spec/puzzle_spec.rb
+++ spec/puzzle_spec.rb
@@ -3,11 +3,25 @@ require('puzzle')
 require('pry')
 
 describe('String#puzzle') do
-  it('will return a string in which the "a" is replaced with "-"') do
-    expect('apex'.puzzle).to(eq('-pex'))
+  it('will return a string in which all instances of "a" are replaced with "-"') do
+    expect('capybara'.puzzle).to(eq('c-pyb-r-'))
   end
-
-
-
-
+  it('will return a string in which all instances of "e" are replaced with "-"') do
+    expect('deer'.puzzle).to(eq('d--r'))
+  end
+  it('will return a string in which all instances of "i" are replaced with "-"') do
+    expect('ink'.puzzle).to(eq('-nk'))
+  end
+  it('will return a string in which all instances of "o" are replaced with "-"') do
+    expect('oasis'.puzzle).to(eq('--s-s'))
+  end
+  it('will return a string in which all instances of "u" are replaced with "-"') do
+    expect('under'.puzzle).to(eq('-nd-r'))
+  end
+  it('will return a vowel as a "-" even if that vowel is capitalized in input') do
+    expect('Antler'.puzzle).to(eq('-ntl-r'))
+  end
+  it('will keep proper punction and capitalization from original string in place and return multi word strings') do
+    expect('Sherman went to the park today!'.puzzle).to(eq('Sh-rm-n w-nt t- th- p-rk t-d-y!'))
+  end
 end
